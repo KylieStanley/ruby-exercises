7e69b0118f10384e1abe1ce283730bf8b38a6f80
Kylie Stanley <knstanley11@gmail.com>
2019-11-17 14:51:12 -0600

    Add vampire and dragon

---
+++ mythical-creatures/lib/dragon.rb
@@ -0,0 +1,20 @@
+class Dragon
+  attr_reader :name, :color, :rider, :meals, :hungry
+
+  def initialize(name, color, rider)
+    @name = name
+    @color = color
+    @rider = rider
+    @meals = 0
+    @hungry = true
+  end
+
+  def hungry?
+    @hungry
+  end
+
+  def eat
+    @meals += 1
+    @hungry = false if @meals === 3 
+  end
+end
