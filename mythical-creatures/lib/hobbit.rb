1da180d0b5baf3beec0f623341a6292f23649908
Kylie Stanley <knstanley11@gmail.com>
2019-11-20 20:51:05 -0600

    Add Hobbit and pass tests

---
+++ mythical-creatures/lib/hobbit.rb
@@ -0,0 +1,33 @@
+class Hobbit
+  attr_reader :name, :disposition, :age
+
+  def initialize(name, disposition = 'homebody', age = 0)
+    @name = name
+    @disposition = disposition
+    @age = age
+  end
+
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def adult?
+    return true if @age > 32
+    false
+  end
+
+  def old?
+    return true if @age > 100
+    false
+  end 
+
+  def has_ring?
+    return true if self.name == 'Frodo'
+    false
+  end
+
+  def is_short?
+    return true
+  end
+
+end
